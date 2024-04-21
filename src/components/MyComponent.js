a47e74044ee16659839d29ee4bb205119086f9c1
Pham Minh Toan <minhtoanpham1412@gmail.com>
2024-04-21 19:07:12 +0700

    nesting component

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,38 +2,22 @@
 // function component
 
 import React from "react";
+import Userinfor from "./Userinfor";
 
 class MyComponent extends React.Component {
 
-    state = {
-        name: "Mizzon",
-        age: 19,
-        address: "HCM"
-    }
-    handleClick(event) {
-        console.log("Click me my button")
-        console.log("Random ", Math.floor((Math.random() * 100) + 1))
-
-        this.setState({
-            name: "Toan",
-            age: Math.floor((Math.random() * 100) + 1)
-        })
-    }
-    handleOnMoverOver(event) {
-        console.log(event.pageX)
-
-    }
 
     // jsx
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm {this.state.age}
-                <button onClick={(event) => { this.handleClick(event) }}>Click me</button>
-                <button onMouseOver={this.handleOnMoverOver}>Mover me</button>
+                <Userinfor></Userinfor>
             </div>
         )
-    }
+
+    };
+
+
 }
 
 export default MyComponent;
