83d52940b49991cff60876946430672b38d141b1
Pham Minh Toan <minhtoanpham1412@gmail.com>
2024-04-21 15:04:19 +0700

    event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -10,11 +10,21 @@ class MyComponent extends React.Component {
         age: 19,
         address: "HCM"
     }
+    handleClick(event) {
+        console.log("Click me my button")
+        console.log(this.state.name)
+    }
+    handleOnMoverOver(event) {
+        console.log(event.pageX)
+    }
+
     // jsx
     render() {
         return (
             <div>
                 My name is {this.state.name}
+                <button onClick={this.handleClick}>Click me</button>
+                <button onMouseOver={this.handleOnMoverOver}>Mover me</button>
             </div>
         )
     }
