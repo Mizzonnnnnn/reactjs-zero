f01996cc441d340fc082a85dfb61e177170e6564
Pham Minh Toan <minhtoanpham1412@gmail.com>
2024-04-25 09:22:29 +0700

    update

--- src/components/Displayinfor.js
+++ src/components/Displayinfor.js
@@ -1,42 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './DisplayInfor.scss';
-
-// class DisplayInfor extends React.Component {
-//     // stateless vs statefull
-//     render() {
-//         console.log(">>> Call me render")
-//         //destructuring array/object
-//         const { listUsers } = this.props;
-//         // console.log(">>>> check props:: ", this.props)
-//         //props => viet tac properties
-//         return (
-//             <div className="display-infor-container">
-
-//                 {
-//                    true &&
-//                     <>
-//                         {listUsers.map((user, index) => {
-//                             return (
-//                                 <div key={user.id} className={+user.age < 17 ? "green" : "red"}>
-//                                     <div>
-//                                         <div>My name is {user.name}</div>
-//                                         <div>My age is {user.age}</div>
-//                                     </div>
-
-//                                     <div>
-//                                         <button onClick={() => { this.props.handleDeleteUser(user.id) }}>de lét </button>
-//                                     </div>
-//                                 </div>
-
-
-//                             )
-//                         })}
-//                     </>
-//                 }
-//             </div>
-//         )
-//     }
-// }
 
 const DisplayInfor = (props) => {
     const { listUsers } = props;
@@ -45,6 +8,18 @@ const DisplayInfor = (props) => {
     const handleShowHideListUser = () => {
         setShowHideListUser(!isShowHideListUser)
     }
+
+    console.log(">>> call me render")
+
+    useEffect(
+        () => {
+            if (listUsers.length === 0) {
+                alert('you delete all users')
+            }
+            console.log(">>> call me useEffect")
+        }, [listUsers] // chỉ chạy 1 lần  thôi
+
+    )
     return (
         <div className="display-infor-container">
             <div>
