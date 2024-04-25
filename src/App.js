6aaa42e27dbcbb1cabecfa8f4265ba1a8a2f1e21
Pham Minh Toan <minhtoanpham1412@gmail.com>
2024-04-25 10:15:58 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
+import Header from './components/Header/Header';
 
 const App = () => {
 
   return (
-    <div className="App">
-      Hello world
+    <div className="app-container">
+      <Header />
     </div>
   );
 };
