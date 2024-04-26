6f2f4116bbfba444ceb0258e5e682f51f0dc9dcf
Pham Minh Toan <minhtoanpham1412@gmail.com>
2024-04-26 23:06:48 +0700

    fix homes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Mizzon</Navbar.Brand>
+                <NavLink to="/" className='navbar-brand'> Mion</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -19,9 +19,9 @@ const Header = () => {
                     </Nav>
                     <Nav>
                         <NavDropdown title="Setting" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+                            <NavDropdown.Item >Log in</NavDropdown.Item>
+                            <NavDropdown.Item>Log out</NavDropdown.Item>
+                            <NavDropdown.It >Profile</NavDropdown.It>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import {
 
 import User from './components/User/User';
 import Admin from './components/Admin/Admin';
+import HomePage from './components/Home/HomePage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -21,6 +22,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
           <Route path="/users" element={<User />} />
           <Route path="/admins" element={<Admin />} />
         </Route>
