5dd668bb1a194ecc125af8f771ff0ef78ebe07e8
Pham Minh Toan <minhtoanpham1412@gmail.com>
2024-05-26 16:26:20 +0700

    xử lý data khi chon cau tra loi

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,18 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
 import _ from "lodash";
 import './DetailQuiz.scss';
+import Question from "./Question";
 
-
-const DetailQuiz = (prop) => {
+const DetailQuiz = () => {
     const params = useParams();
     const location = useLocation();
-
-    console.log(location);
     const quizId = params.id;
-
-
+    const [dataQuiz, setDataQuiz] = useState([]);
+    const [index, setIndex] = useState(0);
 
     useEffect(() => {
         fetchQuestions();
@@ -34,15 +32,53 @@ const DetailQuiz = (prop) => {
                             questionDescription = item.description;
                             image = item.image;
                         }
+                        item.answers.isSelected = false;
                         answers.push(item.answers)
                     })
                     return { questionId: key, answers: answers, questionDescription, image }
                 })
                 .value()
             console.log(data);
+            setDataQuiz(data);
+        }
+    }
+    console.log(">>> check dataQuiz: ", dataQuiz);
+
+    const handlePrev = () => {
+        if (index - 1 < 0) {
+            return;
+        }
+        setIndex(index - 1);
+    }
+
+    const handleNext = () => {
+        if (dataQuiz && dataQuiz.length > index + 1) {
+            setIndex(index + 1);
         }
     }
 
+    const handleCheckbox = (answerId, questionId) => {
+        let dataQuizClone = _.cloneDeep(dataQuiz); // react hool doesn't merge state
+        let question = dataQuizClone.find(item => +item.questionId === +questionId);
+
+        // Đoạn mã này kiểm tra xem question có tồn tại và có answers không. Nếu có, nó sẽ sử dụng map để duyệt qua các câu trả lời
+        if (question && question.answers) {
+            let b = question.answers.map(item => {
+                if (+item.id === +answerId) {
+                    item.isSelected = !item.isSelected;
+                }
+                return item;
+            })
+            question.answers = b;
+        }
+        // Dòng này tìm chỉ mục (index) của câu hỏi trong mảng dataQuizClone dựa trên questionId.
+        let index = dataQuizClone.findIndex(item => +item.questionId === +questionId);
+        // Kiểm tra xem chỉ mục của câu hỏi có tồn tại trong mảng không trước khi cập nhật.
+        if (index > -1) {
+            dataQuizClone[index] = question;
+            setDataQuiz(dataQuizClone);
+        }
+    }
     return (
         <div className="detail-quiz-container">
             <div className="left-content">
@@ -50,22 +86,21 @@ const DetailQuiz = (prop) => {
                     Quiz {quizId} {location?.state?.quizTitle}
                     <hr />
                 </div>
-                <div className="q-body">
-                    <img />
-                </div>
-                <div className="q-content">
-                    <div className="question">Question 1 How are you doing?</div>
 
-                    <div className="answer">
-                        <div>a</div>
-                        <div>b</div>
-                        <div>c</div>
-                        <div>d</div>
-                    </div>
+                <div className="q-content">
+                    <Question
+                        data={
+                            dataQuiz && dataQuiz.length > 0 ?
+                                dataQuiz[index] : []
+                        }
+
+                        handleCheckbox={handleCheckbox}
+                        index={index} />
                 </div>
                 <div className="footer">
-                    <button className="btn btn-primary">Prev</button>
-                    <button className="btn btn-secondary">Next</button>
+                    <button className="btn btn-primary" onClick={() => handlePrev()}>Prev</button>
+                    <button className="btn btn-secondary" onClick={() => handleNext()}>Next</button>
+                    <button className="btn btn-warning" onClick={() => handleNext()}>Finish</button>
                 </div>
             </div>
 
