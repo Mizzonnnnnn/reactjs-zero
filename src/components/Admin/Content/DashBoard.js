443f72a4276405e0e12f4350a3e31cb9c8d64f47
Pham Minh Toan <minhtoanpham1412@gmail.com>
2024-07-15 20:45:10 +0700

    api dashboard

--- src/components/Admin/Content/DashBoard.js
+++ src/components/Admin/Content/DashBoard.js
@@ -1,44 +1,44 @@
-import { Bar, BarChart, CartesianGrid, Legend, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 import './DashBoard.scss'
 import { Tooltip } from 'react-bootstrap';
+import { getOverview } from '../../../services/apiService';
+import { useState, useEffect } from 'react';
 const DashBoard = (props) => {
-    const data = [
-        {
-            "name": "Page A",
-            "uv": 4000,
-            "pv": 2400
-        },
-        {
-            "name": "Page B",
-            "uv": 3000,
-            "pv": 1398
-        },
-        {
-            "name": "Page C",
-            "uv": 2000,
-            "pv": 9800
-        },
-        {
-            "name": "Page D",
-            "uv": 2780,
-            "pv": 3908
-        },
-        {
-            "name": "Page E",
-            "uv": 1890,
-            "pv": 4800
-        },
-        {
-            "name": "Page F",
-            "uv": 2390,
-            "pv": 3800
-        },
-        {
-            "name": "Page G",
-            "uv": 3490,
-            "pv": 4300
+
+    const [dataOverview, setDataOverview] = useState([]);
+    const [dataChart, setDataChart] = useState([]);
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = async () => {
+        let res = await getOverview();
+        if (res && res.EC === 0) {
+            setDataOverview(res.DT);
+            //pprocess chart data
+            let Qz = 0, Qs = 0, As = 0;;
+            Qz = res.DT.others.countQuiz;
+            Qs = res.DT.others.countQuestions;
+            As = res.DT.others.countAnswers;;
+            const data = [
+                {
+                    "name": "Quizzes",
+                    "Qz": Qz,
+                },
+                {
+                    "name": "Questions",
+                    "Qs": Qs,
+                },
+                {
+                    "name": "Answers",
+                    "As": As,
+                }
+            ]
+            setDataChart(data)
         }
-    ]
+        console.log(res)
+    }
     return (
         <div className="dashboard-container">
             <div className="title">
@@ -46,22 +46,56 @@ const DashBoard = (props) => {
             </div>
             <div className="content">
                 <div className='c-left'>
-                    <div className='child'>Total Users</div>
-                    <div className='child'>Total Quizzes</div>
-                    <div className='child'>Total Questions</div>
-                    <div className='child'>Total Answers</div>
+                    <div className='child'>
+                        <span className='title-1'>Total Users</span>
+                        <span className='title-2'>
+                            {
+                                dataOverview && dataOverview.users &&
+                                    dataOverview.users.total ? <>{dataOverview.users.total}</> : <>0</>
+                            }
+                        </span>
+                    </div>
+                    <div className='child'>
+                        <span className='title-1'>Total Quizzes</span>
+                        <span className='title-2'>
+                            {
+                                dataOverview && dataOverview.others &&
+                                    dataOverview.others.countQuiz ? <>{dataOverview.others.countQuiz}</> : <>0</>
+                            }
+                        </span>
+                    </div>
+                    <div className='child'>
+                        <span className='title-1'>Total Questions</span>
+                        <span className='title-2'>
+                            {
+                                dataOverview && dataOverview.others &&
+                                    dataOverview.others.countQuestions ? <>{dataOverview.others.countQuestions}</> : <>0</>
+                            }
+                        </span>
+                    </div>
+                    <div className='child'>
+                        <span className='title-1'>Total Answers</span>
+                        <span className='title-2'>
+                            {
+                                dataOverview && dataOverview.others &&
+                                    dataOverview.others.countAnswers ? <>{dataOverview.others.countAnswers}</> : <>0</>
+                            }
+                        </span>
+                    </div>
                 </div>
                 <div className='c-right'>
-                    <BarChart width={730} height={300} data={data}>
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Bar dataKey="pv" fill="#8884d8" />
-                        <Bar dataKey="uv" fill="#82ca9d" />
-                    </BarChart>
-
+                    <ResponsiveContainer width="95%" height="95%" >
+                        <BarChart width={730} height={300} data={dataChart}>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            {/* <YAxis /> */}
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey="Qz" fill="#8884d8" />
+                            <Bar dataKey="Qs" fill="#82ca9d" />
+                            <Bar dataKey="As" fill="#FFC708" />
+                        </BarChart>
+                    </ResponsiveContainer>
                 </div>
             </div>
         </div>
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -113,6 +113,10 @@ const postUpsertQA = (data) => {
 const logout = (email, refresh_token) => {
     return axios.post('/api/v1/logout', { email, refresh_token })
 }
+
+const getOverview = () => {
+    return axios.get('api/v1/overview')
+}
 export {
     postCreateNewUser,
     getAllUser,
@@ -134,4 +138,5 @@ export {
     getQuizWithQA,
     postUpsertQA,
     logout,
+    getOverview,
 }
